4adbe7448fa23204e7b4db876b6612f399ed2470
szittyu <szittyu1@gmail.com>
2022-04-25 18:27:32 +0200

    link added to the logo

--- components/Navbar.js
+++ components/Navbar.js
@@ -4,9 +4,11 @@ import Image from "next/image";
 const Navbar = () => {
     return (
         <nav className="flex justify-end w-full items-end mx-auto p-2.5 mb-12 border-b-2 border-gray-500 border-solid">
-            <div className="mr-auto">
-                <Image src="/blog.png" width={128} height={110} alt={"blog"} />
-            </div>
+            <Link href="/">
+                <a className="mr-auto">
+                    <Image src="/blog.png" width={128} height={110} alt={"blog"} />
+                </a>
+            </Link>
             <div className="flex justify-between ml-3 w-48 text-gray-600 font-medium">
                 <Link href="/"><a className="hover:text-blue-600">Home</a></Link>
                 <Link href="/about"><a className="hover:text-blue-600">About</a></Link>
